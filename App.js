8d1504fc592e5a4d556daffebac29b2e76282838
ChrisCruze <cruzc09@gmail.com>
2020-09-06 18:09:03 -0400

    enabled anonymous login

--- App.js
+++ App.js
@@ -1,12 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View, TouchableWithoutFeedback } from "react-native";
+import { firebase_sign_in_anonymous } from "./firebase";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <TouchableWithoutFeedback
+        onPress={() => {
+          firebase_sign_in_anonymous();
+        }}
+      >
+        <Text>Login</Text>
+      </TouchableWithoutFeedback>
     </View>
   );
 }
@@ -14,8 +19,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  }
 });
